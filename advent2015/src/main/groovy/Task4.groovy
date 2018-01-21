49fd4449bcb5fc20b2e609d939675d781e89b0ed
uda4nik <yuriy.yermolenko@gmail.com>
2018-01-21 18:12:17 +0100

    solved task 4

---
+++ advent2015/src/main/groovy/Task4.groovy
@@ -0,0 +1,22 @@
+import javax.xml.bind.DatatypeConverter
+import java.security.MessageDigest
+import java.util.function.Predicate
+
+class Task4 {
+    static MessageDigest md5 = MessageDigest.getInstance("MD5")
+
+    static int generate(String input, Predicate<String> condition) {
+        int number = 0
+        while (!condition.test(getHexBinary(input + number))) {
+            number++
+        }
+        return number
+    }
+
+
+    static String getHexBinary(String stringWithNumber) {
+        md5.reset()
+        md5.update(stringWithNumber.getBytes("UTF-8"))
+        DatatypeConverter.printHexBinary(md5.digest())
+    }
+}
---
+++ advent2015/src/test/groovy/Task4Test.groovy
@@ -0,0 +1,31 @@
+import spock.lang.Specification
+import spock.lang.Unroll
+
+import static Task4.getHexBinary
+
+@Unroll
+class Task4Test extends Specification {
+
+    def 'should find number which added will make md5 to return hex with 5 leading zeros'() {
+        when:
+        int number = Task4.generate(given, { it -> it.startsWith("00000") })
+
+        then:
+        number == expectedResult
+
+        and:
+        getHexBinary(given + number).startsWith("00000")
+
+        where:
+        given      | expectedResult
+        "abcdef"   | 609043
+        "pqrstuv"  | 1048970
+        "iwrupvqb" | 346386
+    }
+
+    def 'works for 6 zeros as well'() {
+        expect:
+        Task4.generate("iwrupvqb", { it -> it.startsWith("000000") }) == 9958218
+    }
+
+}
