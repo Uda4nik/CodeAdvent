6f96581940c68f1128b5727093a9875bf1ab1c12
uda4nik <yuriy.yermolenko@gmail.com>
2018-09-28 09:30:45 +0200

    finished part 1 of task 7

--- advent2015/src/main/groovy/Task7.groovy
+++ advent2015/src/main/groovy/Task7.groovy
@@ -4,6 +4,7 @@ class Task7 {
 
     static class WireNet {
         Map<String, Signal> wires = [:]
+        Map<Signal, Integer> cache = [:]
         private LineParser parser = new LineParser()
 
         static WireNet from(String input) {
@@ -18,10 +19,14 @@ class Task7 {
 
         int getSignalOn(String wire) {
             Signal signal = wires[wire]
-            if (signal instanceof ValueSignal) return Integer.valueOf(signal.value)
-            if (signal instanceof AddSignal) return getSignalOn(signal.wire1) & getSignalOn(signal.wire2)
-            if (signal instanceof OrSignal) return getSignalOn(signal.wire1) | getSignalOn(signal.wire2)
-            0
+            if(cache[signal] == null){
+                int result= signal.applyOnWire(this)
+                cache[signal] = result
+                return result
+            } else {
+                return cache[signal]
+            }
+
         }
     }
 
@@ -38,41 +43,115 @@ class Task7 {
         String wire
     }
 
-
-
     static class SignalFactory {
         static Signal from(String input) {
-            if (input.isInteger()) return new ValueSignal(input)
+            if (input.isNumber()) return new ValueSignal(Integer.valueOf(input))
             if (input.contains("AND")) return newAddSignal(input)
             if (input.contains("OR")) return newOrSignal(input)
-            null
+            if (input.contains("SHIFT")) return newShiftSignal(input)
+            if (input.contains("NOT")) return newNotSignal(input)
+            if (!input.isNumber()) return new TransitiveSignal(input)
+            throw new UnsupportedOperationException("Can't parse the command ${input}")
         }
 
-        private static Signal newAddSignal(input){
+        private static Signal newAddSignal(input) {
             String[] split = input.split(" AND ")
-            new AddSignal(wire1: split[0], wire2: split[1])
+            new BitSignal(wire1: split[0], wire2: split[1], type: BitCommandType.AND)
         }
 
-        private static Signal newOrSignal(input){
+        private static Signal newOrSignal(input) {
             String[] split = input.split(" OR ")
-            new OrSignal(wire1: split[0], wire2: split[1])
+            new BitSignal(wire1: split[0], wire2: split[1], type: BitCommandType.OR)
+        }
+
+        private static Signal newShiftSignal(String input) {
+            String[] split = input.split(" .SHIFT ")
+            ShiftType type = input.contains("LSHIFT") ? ShiftType.LEFT : ShiftType.RIGHT
+            new ShiftSignal(wire: split[0], type: type, bitsToShift: Integer.valueOf(split[1]))
+        }
+
+        private static Signal newNotSignal(String input) {
+            String wire = input.substring("NOT ".length())
+            new NotSignal(wire: wire)
         }
     }
 
-    static interface Signal {}
+    static interface Signal {
+        int applyOnWire(WireNet net)
+    }
+
     @Canonical
     static class ValueSignal implements Signal {
-        def value
+        int value
 
+        @Override
+        int applyOnWire(WireNet net) {
+            return value
+        }
     }
 
     @Canonical
-    static class AddSignal implements Signal {
+    static class BitSignal implements Signal {
         String wire1, wire2
+        BitCommandType type
+
+        @Override
+        int applyOnWire(WireNet net) {
+            int leftWire = getValueOf(wire1, net)
+            int rightWire = getValueOf(wire2, net)
+            return type == BitCommandType.AND ? leftWire & rightWire : leftWire | rightWire
+        }
+
+        private getValueOf(String wire, WireNet net) {
+            int result
+            if (wire.isNumber()) {
+                result = Integer.valueOf(wire)
+            } else {
+                result = net.getSignalOn(wire)
+            }
+            result
+        }
     }
 
     @Canonical
-    static class OrSignal implements Signal {
-        String wire1, wire2
+    static class ShiftSignal implements Signal {
+        String wire
+        ShiftType type
+        int bitsToShift
+
+        @Override
+        int applyOnWire(WireNet net) {
+            int signalFromNet = net.getSignalOn(wire)
+            return type == ShiftType.LEFT ? signalFromNet << bitsToShift : signalFromNet >> bitsToShift
+        }
+    }
+
+    @Canonical
+    static class NotSignal implements Signal {
+        String wire
+
+        @Override
+        int applyOnWire(WireNet net) {
+            int signalFromNet = net.getSignalOn(wire)
+            return Short.toUnsignedInt((short) ~signalFromNet)
+        }
+    }
+
+    @Canonical
+    static class TransitiveSignal implements Signal {
+        String wire
+
+        @Override
+        int applyOnWire(WireNet net) {
+            return net.getSignalOn(wire)
+        }
+    }
+
+    enum BitCommandType {
+        AND, OR
+    }
+
+    enum ShiftType {
+        LEFT, RIGHT
     }
 }
--- advent2015/src/test/groovy/Task7Test.groovy
+++ advent2015/src/test/groovy/Task7Test.groovy
@@ -6,7 +6,7 @@ import static Task7.WireNet
 @Unroll
 class Task7Test extends Specification {
 
-    def 'can process signal'() {
+    def 'can process signals'() {
         when:
         WireNet cut = WireNet.from(input)
 
@@ -16,11 +16,27 @@ class Task7Test extends Specification {
         where:
         input                              | wire | output
         "123 -> x"                         | "x"  | 123
+        "123 -> xy"                        | "xy" | 123
+        "123 -> x\nx -> y"                 | "y"  | 123
         "123 -> x\n456 -> y\nx AND y -> d" | "d"  | 72
+        "456 -> y\n123 AND y -> d"         | "d"  | 72
+        "123 -> x\nx AND 456 -> d"         | "d"  | 72
         "123 -> x\n456 -> y\nx OR y -> e"  | "e"  | 507
         "123 -> x\nx LSHIFT 2 -> f"        | "f"  | 492
         "456 -> y\ny RSHIFT 2 -> g"        | "g"  | 114
         "123 -> x\nNOT x -> h"             | "h"  | 65412
         "456 -> y\nNOT y -> i"             | "i"  | 65079
+    }
+
+    def 'integration test 1'() {
+        given:
+        String input = getClass().getResourceAsStream("Task7.text").text
+        WireNet cut = WireNet.from(input)
+
+        when:
+        int result = cut.getSignalOn("a")
+
+        then:
+        result == 3176
     }
 }
